969284055741ee01133c87260d0ffd4319ee3afc
Roland Kopka <kopkaroland@gmail.com>
2020-03-19 16:10:53 +0100

    Add basic seed to FoodContext

--- HealthyLife.Data/FoodContext.cs
+++ HealthyLife.Data/FoodContext.cs
@@ -6,5 +6,12 @@ namespace HealthyLife.Data
     public class FoodContext : DbContext
     {
         public DbSet<Ingredient> Ingredients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient { Name = "Salt"},
+                new Ingredient { Name = "Pepper"});
+        }
     }
 }
