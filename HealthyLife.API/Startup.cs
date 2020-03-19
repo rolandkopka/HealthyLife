f106c10cd8a279630c379bfca66f5762a544031a
Roland Kopka <kopkaroland@gmail.com>
2020-03-19 16:11:56 +0100

    Add FoodContext to API

--- HealthyLife.API/Startup.cs
+++ HealthyLife.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HealthyLife.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace HealthyLife.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<FoodContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("FoodConnex")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
