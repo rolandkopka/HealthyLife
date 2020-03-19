710d0acc1c1d03392f944dc2e731078a714e64a5
Roland Kopka <kopkaroland@gmail.com>
2020-03-19 17:45:25 +0100

    Scaffold Ingredients Controller

---
+++ HealthyLife.API/Controllers/IngredientsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HealthyLife.Data;
+using HealthyLife.Domain.Food;
+
+namespace HealthyLife.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly FoodContext _context;
+
+        public IngredientsController(FoodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ingredients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ingredient>>> GetIngredients()
+        {
+            return await _context.Ingredients.ToListAsync();
+        }
+
+        // GET: api/Ingredients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ingredient>> GetIngredient(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return ingredient;
+        }
+
+        // PUT: api/Ingredients/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIngredient(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ingredient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IngredientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ingredients
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Ingredient>> PostIngredient(Ingredient ingredient)
+        {
+            _context.Ingredients.Add(ingredient);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIngredient", new { id = ingredient.Id }, ingredient);
+        }
+
+        // DELETE: api/Ingredients/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Ingredient>> DeleteIngredient(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ingredients.Remove(ingredient);
+            await _context.SaveChangesAsync();
+
+            return ingredient;
+        }
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredients.Any(e => e.Id == id);
+        }
+    }
+}
