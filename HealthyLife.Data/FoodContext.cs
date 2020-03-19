5bb661dcfb2951436bb2f8d055ed5fb10b34b1dc
Roland Kopka <kopkaroland@gmail.com>
2020-03-19 17:31:07 +0100

    Add Id in seed

--- HealthyLife.Data/FoodContext.cs
+++ HealthyLife.Data/FoodContext.cs
@@ -5,13 +5,17 @@ namespace HealthyLife.Data
 {
     public class FoodContext : DbContext
     {
+        public FoodContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Ingredient> Ingredients { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Ingredient>().HasData(
-                new Ingredient { Name = "Salt"},
-                new Ingredient { Name = "Pepper"});
+                new Ingredient { Id = 1, Name = "Salt"},
+                new Ingredient { Id = 2, Name = "Pepper"});
         }
     }
 }
