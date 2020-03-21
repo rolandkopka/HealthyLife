09f6aae53410d30c0c65be2685e641dde98180dc
Roland Kopka <kopkaroland@gmail.com>
2020-03-21 18:15:35 +0100

    Scaffold Recipe Controller

---
+++ HealthyLife.API/Controllers/RecipesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HealthyLife.Data;
+using HealthyLife.Domain.Food;
+
+namespace HealthyLife.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly FoodContext _context;
+
+        public RecipesController(FoodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Recipes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            return await _context.Recipes.ToListAsync();
+        }
+
+        // GET: api/Recipes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        // PUT: api/Recipes/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(int id, Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recipes
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecipe", new { id = recipe.Id }, recipe);
+        }
+
+        // DELETE: api/Recipes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Recipe>> DeleteRecipe(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return recipe;
+        }
+
+        private bool RecipeExists(int id)
+        {
+            return _context.Recipes.Any(e => e.Id == id);
+        }
+    }
+}
